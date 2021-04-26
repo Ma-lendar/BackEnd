fadf3b650626f750bfe33fca2ee746de9c59906f
conagreen <icoqwee@gmail.com>
2021-04-26 15:45:03 +0900

    cors 설정

--- src/main/java/com/malendar/backend/config/WebConfig.java
+++ src/main/java/com/malendar/backend/config/WebConfig.java
@@ -1,2 +1,26 @@
-package com.malendar.backend.config;public class WebConfig {
+package com.malendar.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+//                .allowedOrigins("http://**")
+//                .allowedOrigins("https://**")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name());
+    }
 }
