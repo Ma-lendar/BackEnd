5808357a0d79c7af63c321a1b03437eedee67260
conagreen <icoqwee@gmail.com>
2021-05-03 13:57:04 +0900

    초기 공통 세팅

--- src/main/java/com/malendar/backend/BackEndApplication.java
+++ src/main/java/com/malendar/backend/BackEndApplication.java
@@ -1,8 +1,14 @@
 package com.malendar.backend;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
+@EnableJpaAuditing
+@RequiredArgsConstructor
 @SpringBootApplication
 public class BackEndApplication {
 
